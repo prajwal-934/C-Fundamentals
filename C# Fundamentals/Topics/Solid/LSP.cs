15f0a6907a5c1d45a76a3175a0903749fd6f7e5d
Prajwal pati <patilprajwal982@gmail.com>
2025-07-02 22:24:00 +0530

    solid : single responsibilty

---
+++ C# Fundamentals/Topics/Solid/LSP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Fundamentals.Topics.Solid
+{
+
+    //lsp- child class instance should be easily subsitute to parent without breaking any code
+    // in below example Ostrich cannot inherit Bird class because Ostrich cannot fly so we can create FlyingBird class
+    internal class Bird
+    {
+        //public void Fly() { }
+    }
+
+    class FlyingBird : Bird { 
+      public void Fly() { }  
+    }
+
+    class Ostrich : Bird { }
+
+    class Pigeon : FlyingBird { }
+
+    
+}
---
+++ C# Fundamentals/Topics/Solid/SolidExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Fundamentals.Topics.Solid
+{
+    internal class OrderProcessor
+    {
+        private readonly OrderValidator orderValidator;
+        private readonly OrderSaver orderSaver;
+        private readonly OrderNotificationSender orderNotificationSender;
+
+        public OrderProcessor(OrderValidator orderValidator, OrderSaver orderSaver, OrderNotificationSender orderNotificationSender)
+        {
+            this.orderValidator = orderValidator;
+            this.orderSaver = orderSaver;
+            this.orderNotificationSender = orderNotificationSender;
+        }
+
+        public void process()
+        {
+            orderValidator.Validate();
+            orderSaver.Save(null);
+            orderNotificationSender.SendNotification();
+        }
+
+    }
+
+    class OrderValidator
+    {
+        public void Validate() { }
+    }
+
+    class OrderSaver
+    {
+        public void Save(string order) { }
+    }
+
+    class OrderNotificationSender
+    {
+        public void SendNotification() { }
+    }
+}
